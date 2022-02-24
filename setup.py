dd685bbc96e47a291b345221b9837fdecc89d083
Tyler887 <tylermageeshields@gmail.com>
2022-02-24 17:57:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from distutils.core import setup
 import py2exe
-self.app_title = "Cross-Platform Package Manager"
-self.title = "Cross-Platform Package Manager"
-
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console = [{'script': "cppm.py"}],
