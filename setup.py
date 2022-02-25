58e47a2623f83a4715b50a9d0c159951bc82042d
Tyler887 <tylermageeshields@gmail.com>
2022-02-25 23:11:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import time
 from colorama import *
 from art import *
 import sys
+import py2exe
+from distutils.core import setup
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console = [{'script': "cppm.py"}],
