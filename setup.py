0a5789409fe5dacf55763dc17614ae05355e4d38
Tyler887 <tylermageeshields@gmail.com>
2022-02-25 23:08:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,9 @@
-print("Importing os module...")
 import os
-try:
-  print("Importing sys module...")
-  import sys
-  print("Importing time module...")
-  import time
-  print("Importing gnupg module...")
-  try:
-    import gnupg
-  except ImportError:
-    print("Package gnupg not installed. Executing pip to request install...")
-    os.system("python -m pip install gnupg")
-    print("Attempting import again...")
-    import gnupg
-  print("Importing platform module...")
-  import platform
-  print("Importing art and colorama modules...")
-  try:
-    from art import *
-  except ImportError:
-    print("Package art not installed. Executing pip to request install...")
-    os.system("python -m pip install art")
-    print("Attempting import again...")
-    from art import *
-  try:
-    from colorama import *
-  except ImportError:
-    print("Package colorama not installed. Executing pip to request install...")
-    os.system("python -m pip install colorama")
-    print("Attempting import again...")
-    from colorama import *
-  os.system('cls' if os.name=="nt" else 'clear') # else coloring will obviously not work on windows console host
-  from distutils.core import setup
-  import py2exe
-except:
-    print("An error has occured.")
-    exit(1)
+import gnupg
+import time
+from colorama import *
+from art import *
+import sys
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console = [{'script': "cppm.py"}],
